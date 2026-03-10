3f9889abd636c344cba3210ee739d0a6259fc250
688101002-art <688101002@mail.pbru.ac.th>
2026-03-10 21:46:55 +0700

    Create Child1.c

---
+++ C-codes/Child1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main() {
+	printf("My bio.: \n");
+	printf("ID : 688101002 \n");
+	printf("Name : Rungnapa Sookgoen\n");
+	printf("------------------------------\n");
+	getchar();
+}
